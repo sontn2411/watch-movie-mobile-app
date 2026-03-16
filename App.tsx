08b0857ab7ec917857f4759987d557ba59ac83d3
Tran Ngoc son <son.tran@sweetsoft.vn>
2026-03-16 18:28:26 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -6,11 +6,12 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import './global.css';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,6 +29,11 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
+      <View className="bg-blue-200 p-4 m-4 rounded-xl items-center justify-center">
+        <Text className="text-xl font-bold text-blue-800">
+          Tailwind CSS is Working! 🚀
+        </Text>
+      </View>
       <NewAppScreen
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
