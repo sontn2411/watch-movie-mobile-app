7bbcc115dad664004f1c0ebf4d3d43703e172581
Tran Ngoc son <son.tran@sweetsoft.vn>
2026-03-18 19:30:22 +0700

    chore : screen splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -6,22 +6,31 @@ import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles/global.css'; // Global styles
 import { globalThemeVars } from '@/constants/themeVars';
+import BootSplash from 'react-native-bootsplash';
 import { COLORS } from '@/constants/theme';
 import RootNavigator from '@/navigation/RootNavigator';
+import { AnimatedBootSplash } from '@/components/AnimatedBootSplash';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [appReady, setAppReady] = useState(false);
+  const [splashVisible, setSplashVisible] = useState(true);
+
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
-        <NavigationContainer theme={DarkTheme}>
+        <NavigationContainer theme={DarkTheme} onReady={() => setAppReady(true)}>
           <View style={globalThemeVars} className="flex-1">
             <StatusBar barStyle="light-content" backgroundColor={COLORS.background} />
             <RootNavigator />
           </View>
         </NavigationContainer>
       </QueryClientProvider>
+      
+      {appReady && splashVisible && (
+        <AnimatedBootSplash onAnimationEnd={() => setSplashVisible(false)} />
+      )}
     </SafeAreaProvider>
   );
 }
